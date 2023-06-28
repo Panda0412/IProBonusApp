26499b211d861ef5227682427c7ae35c20d99745
Panda0412 <nasb_99@mail.ru>
2023-06-28 21:15:34 +0600

    Initial Commit

---
+++ IProBonusApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IProBonusApp
+//
+//  Created by Anastasiia Bugaeva on 28.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
